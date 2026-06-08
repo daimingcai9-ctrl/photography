f25c26fd6e84c99660cf16cb70cdcbc130f45b53
caidaiming <daimingcai9@gmail.com>
2026-06-08 10:29:56 +0800

    Prepare for Cloudflare Pages: remove unnecessary use client directives, add cache headers

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { getAllPhotos } from "@/lib/photos";
 import {
   MonthlyChart,
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Hero from "@/components/Hero";
 import { getAllPhotos } from "@/lib/photos";
 
